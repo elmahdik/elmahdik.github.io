ae139fec3435ec81b0c21479a76fb1f343744bfe
ElmahdiK <elmahdi.korfed@gmail.com>
2024-08-10 09:11:55 +0200

    feat : emailjs added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,10 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://elmahdik.github.io',
-    // output: "hybrid", // or 'server'
-    // experimental: {
-    //     actions: true,
-    // },
+    output: "hybrid", // or 'server'
+    experimental: {
+        actions: true,
+    },
     integrations: [
         react({
             experimentalReactChildren: true,
