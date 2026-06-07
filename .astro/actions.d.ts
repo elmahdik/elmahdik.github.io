0d56ce3f060693f54082a6f2cb953c6817c8dab2
ElmahdiK <elmahdi.korfed@gmail.com>
2026-06-07 11:52:50 +0200

    fix: update import path in actions.d.ts and correct project label in projects.json

--- .astro/actions.d.ts
+++ .astro/actions.d.ts
@@ -1,5 +1,5 @@
 declare module "astro:actions" {
-	type Actions = typeof import("/Users/ek_mac/Documents/_portfolio/portfolio-astro/src/actions")["server"];
+	type Actions = typeof import("/Users/ek_mac/Documents/portfolio/elmahdik.github.io/src/actions")["server"];
 
 	export const actions: Actions;
 }
