4f595a87371ec33f239c16acbbd70f6efe11ce46
ElmahdiK <elmahdi.korfed@gmail.com>
2026-06-07 11:26:34 +0200

    feat: update contact information and enhance portfolio layout

--- .astro/actions.d.ts
+++ .astro/actions.d.ts
@@ -1,5 +1,5 @@
 declare module "astro:actions" {
-	type Actions = typeof import("/Users/ek_mac/Documents/elmahdik.github.io/src/actions")["server"];
+	type Actions = typeof import("/Users/ek_mac/Documents/_portfolio/portfolio-astro/src/actions")["server"];
 
 	export const actions: Actions;
 }
